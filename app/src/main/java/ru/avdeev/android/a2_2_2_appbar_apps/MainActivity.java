41ddaeeb25d47c6b0dee9333ae1a3b76aac0cf50
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2020-09-14 18:39:24 +0300

    Does not work

--- app/src/main/java/ru/avdeev/android/a2_2_2_appbar_apps/MainActivity.java
+++ app/src/main/java/ru/avdeev/android/a2_2_2_appbar_apps/MainActivity.java
@@ -1,12 +1,23 @@
 package ru.avdeev.android.a2_2_2_appbar_apps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_notes) {
+            //Toast.makeText(MainActivity.this, getText(R.string.open_netbook), Toast.LENGTH_LONG).show();
+            Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+            startActivity(intentNotes);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/ru/avdeev/android/a2_2_2_appbar_apps/NotesActivity.java
@@ -0,0 +1,37 @@
+package ru.avdeev.android.a2_2_2_appbar_apps;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotesActivity extends AppCompatActivity {
+
+    private EditText editTextNote;
+    private final static String KEY = "key";
+    private final static String TEXTNOTE = "text_note";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextNote = findViewById(R.id.notebookEditText);
+        editTextNote.setText(loadNoteText());
+    }
+
+    public void btnSaveClicked (View view) {
+        saveNoteText(editTextNote.getText().toString());
+    }
+
+    private void saveNoteText (String text) {
+        getSharedPreferences(TEXTNOTE,MODE_PRIVATE)
+                .edit()
+                .putString(KEY,text)
+                .apply();
+    }
+
+    private String loadNoteText() {
+        return getSharedPreferences(TEXTNOTE,MODE_PRIVATE).getString(KEY,"");
+    }
+}
